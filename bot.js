81ed19756b23bef81c4e439c1cc8cda1a4f73d11
dashimaki <akira.odashima.sys@gmail.com>
2020-07-25 02:54:57 +0900

    added weather-app function base

--- bot.js
+++ bot.js
@@ -1,15 +1,47 @@
+const Discord = require("discord.js");
+const Unirest = require("unirest");
 const config = require("./config/settings");
 
-const Discord = require("discord.js");
 const client = new Discord.Client();
-
 client.on("ready", () => {
   console.log("ログインしました。");
 });
 
-client.on("message", msg => {
-  if (msg.content.startsWith("!ping")) {
-    msg.channel.send("pong");
+client.on("message", message => {
+  /* example */
+  if (message.content.startsWith("!ping")) {
+    message.channel.send("pong");
+  }
+
+  /* weather-app */
+  if (message.content.startsWith("!weather")) {
+    var req = Unirest(
+      "GET",
+      "https://community-open-weather-map.p.rapidapi.com/weather"
+    );
+
+    req.query({
+      callback: "test",
+      id: "2172797",
+      units: "metric",
+      mode: "html",
+      q: "London,uk"
+    });
+
+    req.headers({
+      "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+      "x-rapidapi-key": "90425a17e6msh7b40496a6abe1a2p19f251jsn0244d05b3b97",
+      useQueryString: true
+    });
+
+    req.end(res => {
+      if (res.error) {
+        throw new Error(res.error);
+      }
+
+      message.channel.send(res.status);
+      message.channel.send(res.body);
+    });
   }
 });
 
