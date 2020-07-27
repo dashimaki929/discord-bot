6fc544cf0166d22f15c4940f8b8ae74c630f0da3
dashimaki <akira.odashima.sys@gmail.com>
2020-07-27 21:42:47 +0900

    added link-collector

--- bot.js
+++ bot.js
@@ -4,13 +4,34 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("ready", () => {
-  console.log("ログインしました。");
+  console.log("Boot completed successfully!");
 });
 
 client.on("message", message => {
   if (message.content.startsWith("!ping")) {
     message.channel.send("pong");
   }
+
+  if (message.content.includes("http")) {
+    if (message.author.id === config.settings.Bot.id) {
+      return;
+    }
+
+    const author = message.author.username;
+    client.channels
+      .fetch(config.settings.Output.textChannel.id)
+      .then(channel => {
+        channel.send(
+          `${message.content
+            .split("\n")
+            .map(line => {
+              return "> " + line;
+            })
+            .join("\n")}\n\nBy: \`${author}\``
+        );
+      })
+      .catch(console.error);
+  }
 });
 
-client.login(config.settings.botToken);
+client.login(config.settings.Bot.token);
--- config/settings.js
+++ config/settings.js
@@ -1,3 +1,12 @@
 exports.settings = {
-    botToken: "NzM2MjYwODI0MTUwMzEwOTI0.XxsOUw.kQtCrqH4bqVm5KeErqQ-IzvMTyA"
-}
+  Bot: {
+    id: "736260824150310924",
+    token: "NzM2MjYwODI0MTUwMzEwOTI0.XxsOUw.kQtCrqH4bqVm5KeErqQ-IzvMTyA"
+  },
+  Output: {
+    textChannel: {
+      id: "737264070763479071" // 尊師v2
+    //   id: "736258211182674044" // Develop
+    }
+  }
+};
