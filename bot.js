22671b51cc269d9498a06268e068f6c2b61fa81b
dashimaki929 <akira.odashima.sys@gmail.com>
2020-08-01 00:16:27 +0900

    refactoring link-stock bot message

--- bot.js
+++ bot.js
@@ -13,30 +13,54 @@ client.on("message", message => {
   }
 
   if (message.content.includes("http")) {
-
-    console.log(message.channel.id);
+    const interests = [
+      "twitter",
+      "youtube",
+      "youtu.be",
+      "steam",
+      "gyazo",
+      "streamable",
+      "amazon",
+      "nicovideo",
+      "instagram",
+      "dashimaki"
+    ];
 
     if (
       message.author.id === config.settings.Bot.id ||
-      message.channel.id === config.settings.Output.textChannel.id
+      message.channel.id === config.settings.Output.textChannel.id ||
+      message.channel.id === config.settings.Input.textChannel.id
     ) {
       return;
     }
 
-    const author = message.author.username;
+    const matchedTags = interests.filter(item =>
+      message.content.includes(item)
+    );
+
     client.channels
       .fetch(config.settings.Output.textChannel.id)
       .then(channel => {
         channel.send(
+          // テキストメッセージを引用
           `${message.content
             .split("\n")
-            .map(line => {
-              return "> " + line;
-            })
-            .join("\n")}\n\nBy: \`${author}\``
+            .map(line => `> ${line}`)
+            .join("\n")}\n\n` +
+          // タグ表示
+            `${matchedTags.map(item => `\`#${item}\``).join(" ")}\n` +
+          // 投稿者へメンション
+            `Added by: \<@${message.author.id}\>`
         );
       })
       .catch(console.error);
+  }
+
+  if (message.content.startsWith("!debug")) {
+    console.log(message);
+    message.channel.send(
+      "```\n" + JSON.stringify(message, null, "\t") + "\n```"
+    );
   }
 });
 
--- config/settings.js
+++ config/settings.js
@@ -3,9 +3,14 @@ exports.settings = {
     id: "736260824150310924",
     token: "NzM2MjYwODI0MTUwMzEwOTI0.XxsOUw.kQtCrqH4bqVm5KeErqQ-IzvMTyA"
   },
+  Input: {
+    textChannel: {
+      id: "633016031966658565" // 尊師v2 - console
+    }
+  },
   Output: {
     textChannel: {
-      id: "737264070763479071" // 尊師v2
+      id: "737264070763479071" // 尊師v2 - link-stock
     //   id: "736258211182674044" // Develop
     }
   }
